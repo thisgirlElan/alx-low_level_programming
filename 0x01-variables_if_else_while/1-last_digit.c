35483b6df4de81a951ab0eef02efc1c5de2d466c
Elan <elankatheryne@gmail.com>
2023-04-24 14:50:36 +0300

    Fix Betty Coding Style

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,7 +7,6 @@
  * greater than 5, 0, or less than 6
  * Return: 0
  */
- 
 int main(void)
 {
 	int n;
@@ -15,7 +14,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+
 	lastDigit = n % 10;
 	printf("Last digit of %i is %i and is ", n, lastDigit);
 	if (lastDigit > 5)
