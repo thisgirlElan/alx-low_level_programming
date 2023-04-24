bb4e52f97547777021b3a1bbb936a1dd6f9d9788
Elan <elankatheryne@gmail.com>
2023-04-24 19:04:25 +0300

    Fix betty coding style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
 * main - prints text and a new line with printf
 *
-* Return: 0 
+* Return: 0
 */
 int main(void)
 {
 
-printf("with proper grammar, but the outcome is a piece of art, \n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 
 return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
 * main - prints text and a new line
 *
-* Return: 0 
+* Return: 0
 */
 
 int main(void)
