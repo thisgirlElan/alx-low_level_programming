b7d1f8989735312ed451e880f5ad7a078d8eeb8f
Elan <elankatheryne@gmail.com>
2023-04-24 19:00:10 +0300

    Fix trailing white space

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
 * main - prints text and a new line with put
 *
-* Return: 0 
+* Return: 0
 */
-int main (void)
+int main(void)
 {
 
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 
 return (0);
 }
