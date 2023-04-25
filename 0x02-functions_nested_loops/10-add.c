0c5813f3392b07b5d85578d88cf9dc2a0ebadca8
Elan <elankatheryne@gmail.com>
2023-04-25 15:11:29 +0300

    return sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* add - adds to integers and returns result
+*@a: first value
+*@b: second value
+*
+* Return: sum
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
