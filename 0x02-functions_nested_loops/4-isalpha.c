5f298500b940d89806408964d97ab4feaa01b272
Elan <elankatheryne@gmail.com>
2023-04-25 13:43:13 +0300

    check for letters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _isalpha - chacks for aplphabets
+* @c: represents character being checked
+*
+* Return: 1 if c is a letter else, 0
+*/
+
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') ||
+(c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
