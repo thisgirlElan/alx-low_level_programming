51f424d4580b7cc111ac9d31f247a9e3fc8b8304
Elan <elankatheryne@gmail.com>
2023-04-26 15:11:32 +0300

    Fix for loop

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,10 +15,10 @@ int prod;
 
 if (n >= 0 && n <= 15)
 {
-for (num = 0; num < n; num++)
+for (num = 0; num <= n; num++)
 {
 _putchar('0');
-for (mult = 0; mult < n; mult++)
+for (mult = 0; mult <= n; mult++)
 {
 _putchar(',');
 _putchar(' ');
