a11d09d4a9fd6ad2c69880d5eb28455e406126c2
Elan <elankatheryne@gmail.com>
2023-04-26 15:21:37 +0300

    Fix loc

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,18 +1,13 @@
 #include "main.h"
 
 /**
-* print_times_table - prints multiples of n
-*prints nothing if n > 15 || n < 0
+* print_times_table - prints multiples of n, nothing if n > 15 || n < 0
 *@n: represents value
-*
 * Return: 0
 */
 void print_times_table(int n)
 {
-int num;
-int mult;
-int prod;
-
+int num, mult, prod;
 if (n >= 0 && n <= 15)
 {
 for (num = 0; num <= n; num++)
@@ -23,13 +18,10 @@ for (mult = 1; mult <= n; mult++)
 _putchar(',');
 _putchar(' ');
 prod = num * mult;
-
 if (prod <= 99)
 _putchar(' ');
-
 if (prod <= 9)
 _putchar(' ');
-
 if (prod >= 100)
 {
 _putchar((prod / 100) + '0');
@@ -39,17 +31,9 @@ else if (prod <= 99 && prod >= 10)
 {
 _putchar((prod / 10) + '0');
 }
-else
-{
 _putchar((prod % 10) + '0');
 }
-
-}
-
 _putchar('\n');
-
 }
-
 }
-
 }
