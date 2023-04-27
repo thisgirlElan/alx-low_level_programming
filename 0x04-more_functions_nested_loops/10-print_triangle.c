3a8f9383c4c186efbb19d9a37b084e00a8b93bf5
Elan <elankatheryne@gmail.com>
2023-04-28 01:52:09 +0300

    Fix betty coding style

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,33 +1,32 @@
 #include "main.h"
 
 /**
- * print_triangle - prints the size if > 0 else, newline
- * @size: size of the triangle
- * Return: Always 0
+* print_triangle - prints the size if > 0 else, newline
+* @size: size of the triangle
+* Return: Always 0
 */
 void print_triangle(int size)
 {
-    int row, hash, space;
+int row, hash, space;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (row = 1; row <= size; row++)
+{
+for (space = size - row; space >= 1; space--)
+{
+_putchar(' ');
+}
+for (hash = 1; hash <= row; hash++)
+{
+_putchar('#')
+}
+_putchar('\n')
+}
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        for (row = 1; row <= size; row++)
-        {
-            for (space = size - row; space >= 1; space--)
-            {
-                _putchar(' ');
-            }
-            for (hash = 1; hash <= row; hash++)
-            {
-                _putchar('#')
-            }
-            _putchar('\n')
-        }
-        
-    }
-    
+}
+
 }
