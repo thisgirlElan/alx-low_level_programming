ff39cb86ba4e2478044eb55c0662fd7ac02eaf0a
Elan <elankatheryne@gmail.com>
2023-05-04 02:24:12 +0300

    Fix betty

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,15 +8,15 @@
 *
 * Return: 0 succes.
 */
-
 void print_array(int *a, int n)
 {
 int i;
 for (i = 0; i < n; i++)
 {
 printf("%d", a[i]);
-if (i < n - 1) {
-    printf(", ");
+if (i < n - 1)
+{
+printf(", ");
 }
 }
 printf("\n");
