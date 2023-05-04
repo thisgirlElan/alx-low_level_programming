e618df7ddf3256bd3346b30c2ee27477c66cb9f9
Elan <61628746+thisgirlElan@users.noreply.github.com>
2023-05-04 04:34:24 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -16,13 +16,19 @@ int i, rand_num;
 
 srand(time(NULL));
 
-for (i = 0; i < PASSWORD_LENGTH; i++) {
+for (i = 0; i < PASSWORD_LENGTH; i++)
+{
 rand_num = rand() % 62;
-if (rand_num < 26) {
+if (rand_num < 26)
+{
 password[i] = 'a' + rand_num;
-} else if (rand_num < 52) {
+}
+else if (rand_num < 52)
+{
 password[i] = 'A' + rand_num - 26;
-} else {
+}
+else
+{
 password[i] = '0' + rand_num - 52;
 }
 }
@@ -31,5 +37,5 @@ password[PASSWORD_LENGTH] = '\0';
 
 printf("%s\n", password);
 
-return 0;
+return (0);
 }
