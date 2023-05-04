009ec8aa5c174b53e00459bf4629c5a8b50c452c
Elan <elankatheryne@gmail.com>
2023-05-04 03:51:05 +0300

    Password generator

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 8
+
+/**
+ * main - generates random password
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int i, password[PASSWORD_LENGTH];
+
+srand((unsigned int)time(NULL));
+
+for (i = 0; i < PASSWORD_LENGTH; i++)
+{
+password[i] = rand() % 94 + 33;
+}
+
+printf("Password: ");
+for (i = 0; i < PASSWORD_LENGTH; i++)
+{
+printf("%c", password[i]);
+}
+printf("\n");
+
+return 0;
+}
