9be27a04794e879b19f1bb30f7ccf4c1a345695c
Elan <elankatheryne@gmail.com>
2023-05-06 02:40:09 +0300

    minor fixes

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,59 +2,45 @@
 #include <stdio.h>
 
 /**
-* print_buffer -  print the content of size bytes of the buffer pointed by b
-* @b: destination string
-* @size: source string
+* print_buffer - prints the content of a buffer
+* @b: pointer to the buffer
+* @size: size of the buffer
 *
-* Return: 0
+* Return: void
 */
-
 void print_buffer(char *b, int size)
 {
 int i, j;
 
-if (size <= 0)
-{
-printf("\n");
-return (0);
-}
-
 for (i = 0; i < size; i += 10)
 {
 printf("%08x: ", i);
-for (j = 0; j < 10; j++)
+
+for (j = i; j < i + 10; j++)
 {
-if ((i+j) < size)
-{
-printf("%02x", b[i+j]);
-}
+if (j < size)
+printf("%02x", b[j]);
+
 else
-{
 printf("  ");
-}
+
 if (j % 2 == 1)
-{
 printf(" ");
 }
-}
-for (j = 0; j < 10; j++)
+for (j = i; j < i + 10; j++)
 {
-if ((i+j) < size)
-{
-if (b[i+j] >= 32 && b[i+j] <= 126)
-{
-printf("%c", b[i+j]);
-}
+if (j >= size)
+break;
+
+if (b[j] >= ' ' && b[j] <= '~')
+putchar(b[j]);
+
 else
-{
-printf(".");
+putchar('.');
+
 }
+putchar('\n');
 }
-else
-{
-printf(" ");
-}
-}
-printf("\n");
-}
+if (size <= 0)
+putchar('\n');
 }
