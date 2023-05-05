695fa28701a99085e830a688c8b4861b3831a07f
Elan <elankatheryne@gmail.com>
2023-05-06 02:26:12 +0300

    Encode string

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* char *rot13 -encodes a string using rot13
+* @s: character
+*/
+
+char *rot13(char *s)
+{
+char *p = s;
+while (*p)
+{
+if ((*p >= 'a' && *p <= 'm') || (*p >= 'A' && *p <= 'M'))
+*p += 13;
+else if ((*p >= 'n' && *p <= 'z') || (*p >= 'N' && *p <= 'Z'))
+*p -= 13;
+p++;
+}
+return (s);
+}
