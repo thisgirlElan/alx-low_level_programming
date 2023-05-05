2562f49a07c43f4202716db06f0ad7c0271a24d0
Elan <elankatheryne@gmail.com>
2023-05-06 02:33:29 +0300

    Fix betty

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -15,29 +15,29 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 int len1 = strlen(n1);
 int len2 = strlen(n2);
 int i, j, k = 0, carry = 0;
-    
+
 for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0; i--, j--)
 {
 int d1 = (i >= 0) ? (n1[i] - '0') : 0;
 int d2 = (j >= 0) ? (n2[j] - '0') : 0;
 int sum = d1 + d2 + carry;
-        
+
 if (k >= size_r - 1 || (sum >= 10 && k >= size_r - 2))
 {
 return (0);
 }
-        
+
 r[k++] = sum % 10 + '0';
 carry = sum / 10;
 }
-    
+
 if (carry && k < size_r - 1)
 {
 r[k++] = carry + '0';
 }
-    
+
 r[k] = '\0';
-    
+
 for (i = 0, j = k - 1; i < j; i++, j--)
 {
 char temp = r[i];
