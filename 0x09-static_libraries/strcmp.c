21cf30640f0ad1ef072bb42a498389a015ce5f5b
Elan <elankatheryne@gmail.com>
2023-05-15 12:39:02 +0300

    Fix betty

--- 0x09-static_libraries/strcmp.c
+++ 0x09-static_libraries/strcmp.c
@@ -9,15 +9,18 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-        int index = 0;
+int index = 0;
 
-        while (s1[index] != '\0' && s2[index] != '\0' && s1[index] == s2[index]) {
-                index++;
-        }
+while (s1[index] != '\0' && s2[index] != '\0' && s1[index] == s2[index])
+{
+        index++;
+}
 
-        if (s1[index] == s2[index]) {
-                return (0);
-        } else {
-                return (s1[index] - s2[index]);
-        }
+if (s1[index] == s2[index])
+{
+return (0);
+} else
+{
+return (s1[index] - s2[index]);
+}
 }
