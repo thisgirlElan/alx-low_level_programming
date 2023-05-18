28b2239090ea4e8a3e282ed7401c4e7d88762288
Elan <elankatheryne@gmail.com>
2023-05-19 01:38:06 +0300

    fix betty

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,41 +4,48 @@
 
 /**
 * main - multiplies two positive numbers
-* Return: success
+* Return: success 0
 */
 
-int validate_arguments(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Error\n");
-        return 0;
-    }
-
-    for (int i = 1; i < 3; i++) {
-        for (int j = 0; argv[i][j] != '\0'; j++) {
-            if (!isdigit(argv[i][j])) {
-                printf("Error\n");
-                return 0;
-            }
-        }
-    }
-
-    return 1;
+int validate_arguments(int argc, char *argv[])
+{
+if (argc != 3)
+{
+printf("Error\n");
+return (0);
 }
 
-unsigned long long multiply_numbers(unsigned long long num1, unsigned long long num2) {
-    return num1 * num2;
+for (int i = 1; i < 3; i++)
+{
+for (int j = 0; argv[i][j] != '\0'; j++)
+{
+if (!isdigit(argv[i][j]))
+{
+printf("Error\n");
+return (0);
+}
+}
 }
 
-int main(int argc, char *argv[]) {
-    if (!validate_arguments(argc, argv))
-        return 98;
+return (1);
+}
 
-    unsigned long long num1 = strtoull(argv[1], NULL, 10);
-    unsigned long long num2 = strtoull(argv[2], NULL, 10);
+unsigned long long multiply_numbers(unsigned long long num1, unsigned long long num2)
+{
+return (num1 * num2);
+}
 
-    unsigned long long result = multiply_numbers(num1, num2);
+int main(int argc, char *argv[])
+{
+if (!validate_arguments(argc, argv))
+return (98);
 
-    printf("%llu\n", result);
+unsigned long long num1 = strtoull(argv[1], NULL, 10);
+unsigned long long num2 = strtoull(argv[2], NULL, 10);
 
-    return 0;
+unsigned long long result = multiply_numbers(num1, num2);
+
+printf("%llu\n", result);
+
+return (0);
 }
