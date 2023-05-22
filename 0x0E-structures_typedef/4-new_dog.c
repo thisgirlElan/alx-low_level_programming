40e46fa4b8deef9dffac393feddd6bb4af9d16b5
Elan <elankatheryne@gmail.com>
2023-05-22 22:56:59 +0300

    minor fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,48 +4,6 @@
 #include "dog.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: the string to get the length of
- *
- * Return: the length of the string
- */
-int _strlen(char *s)
-{
-int len = 0;
-
-while (*s != '\0')
-{
-len++;
-s++;
-}
-
-return (len);
-}
-
-/**
-* _strcpy - copies src string and returns dest pointer
-* @dest: dest char
-* @src: src char
-*
-* Return: pointer to dest.
-*/
-
-char *_strcpy(char *dest, char *src)
-{
-int i = 0;
-while (src[i] != '\0')
-{
-dest[i] = src[i];
-i++;
-}
-
-dest[i] = '\0';
-
-return (dest);
-
-}
-
-/**
 * dog_t - creates a new dog
 * @name: name
 * @age: age
@@ -67,8 +25,8 @@ if (newDog == NULL)
 return (NULL);
 }
 
-newDog->name = malloc(_strlen(name) + 1);
-newDog->owner = malloc(_strlen(owner) + 1);
+newDog->name = malloc(strlen(name) + 1);
+newDog->owner = malloc(strlen(owner) + 1);
 
 if (newDog->name == NULL || newDog->owner == NULL)
 {
@@ -78,8 +36,8 @@ free(newDog);
 return (NULL);
 }
 
-_strcpy(newDog->name, name);
-_strcpy(newDog->owner, owner);
+strcpy(newDog->name, name);
+strcpy(newDog->owner, owner);
 newDog->age = age;
 
 return (newDog);
