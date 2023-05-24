3952a58f6fa5ede815b05ed6450ce13e81b8ddaf
Elan <elankatheryne@gmail.com>
2023-05-24 19:23:53 +0300

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+* print_name - Prints name
+* @name: name
+* @f: pointer to function
+*
+* Return: 0 on success
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+if (!name || !f)
+return;
+
+f(name);
+}
