07b81fac745cba58726805580e5850c9c70ab134
Elan <elankatheryne@gmail.com>
2023-05-25 16:57:43 +0300

    minor fixes

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,17 +1,14 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 /**
-* get_op_func - Selects the correct function to perform the operation
-* asked by the user
-* @s: Operator passed as an argument
+* get_op_func - selects the correct function to perform the
+* operation asked by the user.
+* @s: The operator passed as an argument
 *
-* Return: A pointer to the function that corresponds to the operator given
-* as a parameter
+* Return: A pointer to the corresponding function,
+* or NULL if the operator is invalid
 */
-
 int (*get_op_func(char *s))(int, int)
 {
 op_t ops[] = {
@@ -24,12 +21,12 @@ op_t ops[] = {
 };
 int i = 0;
 
-while (ops[i].op != NULL)
+while (ops[i].op)
 {
-if (*(ops[i].op) == *s && *(s + 1) == '\0')
+if (*(ops[i].op) == *s)
 return (ops[i].f);
 i++;
 }
 
-return (0);
+return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -4,17 +4,15 @@
 
 /**
 * main - Entry point
-* @argc: Number of command-line arguments
-* @argv: Array containing the command-line arguments
+* @argc: The number of command-line arguments
+* @argv: An array containing the command-line arguments
 *
-* Return: 0 on success, 98 for wrong number of arguments,
-* 99 for unknown operator, 100 for division/modulo by 0.
+* Return: 0 on success, or the corresponding error code
 */
-
 int main(int argc, char *argv[])
 {
-int num1, num2, result;
-int (*func)(int, int);
+int num1, num2;
+int (*op_func)(int, int);
 
 if (argc != 4)
 {
@@ -24,25 +22,21 @@ return (98);
 
 num1 = atoi(argv[1]);
 num2 = atoi(argv[3]);
+op_func = get_op_func(argv[2]);
 
-if (argv[2][0] == '/' || argv[2][0] == '%')
-{
-if (num2 == 0)
-{
-printf("Error\n");
-return (100);
-}
-}
-
-func = get_op_func(argv[2]);
-if (func == NULL)
+if (op_func == NULL || argv[2][1] != '\0')
 {
 printf("Error\n");
 return (99);
 }
 
-result = func(num1, num2);
-printf("%d\n", result);
+if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+{
+printf("Error\n");
+return (100);
+}
+
+printf("%d\n", op_func(num1, num2));
 
 return (0);
 }
