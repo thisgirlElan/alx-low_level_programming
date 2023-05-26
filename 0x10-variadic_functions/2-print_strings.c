588459e4fe612d0629aa253851ec9c02ffae65d0
Elan <elankatheryne@gmail.com>
2023-05-27 00:00:03 +0300

    add betty documentation

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,5 +1,11 @@
 #include "variadic_functions.h"
 
+/**
+* print_strings - prints strings
+* @separator: pointer param
+* @n: param
+*/
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 va_list args;
