71603d3fd9cd9721ce8c4de8246c8560362d50a0
Elan <elankatheryne@gmail.com>
2023-05-26 23:39:16 +0300

    minor fixes

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,11 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
-#include <stddef.h>
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int main(int argc, char *argv[]);
 
-#endif /* MAIN_H*/
+#endif
