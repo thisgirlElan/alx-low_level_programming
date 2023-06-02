d2ee0ad09ffde591b7d057be33f60952ce113e62
Elan <elankatheryne@gmail.com>
2023-06-03 01:15:42 +0300

    Fix betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * before_main - Function executed before main()
+ *
+ * It's as a constructor and is automatically
+ * executed before the main()
+ */
+
 void __attribute__((constructor)) before_main()
 {
 printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
