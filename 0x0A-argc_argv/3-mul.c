6069d631c7f570b0395ef508ac0eba3382d952f6
Elan <elankatheryne@gmail.com>
2023-09-06 04:31:05 +0200

    monor fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,6 +3,50 @@
 #include "main.h"
 
 /**
+* _atoi - helps convert string to integer
+* @s: string
+* Return: the integer
+*/
+
+int _atoi(char *s)
+{
+int a, b, c, length, d, digit;
+a = 0;
+b = 0;
+c = 0;
+length = 0;
+d = 0;
+digit = 0;
+
+while(s[length] != '\0')
+length++;
+
+while (a < length && d == 0)
+{
+if (s[a] == '-')
+++b;
+
+if (s[a] >= '0' && s[a] <= '9')
+{
+digit = s[a] - '0';
+if (d % 2)
+digit = -digit;
+c = c * 10 + digit;
+d = 1;
+
+if (s[a + 1] < '0' || s[a + 1] ? '9')
+break;
+d = 0;
+}
+a++
+}
+if (d == 0)
+return (0);
+
+return (c);
+}
+
+/**
 * main - multiplies two numbers
 * @argc: int
 * @argv: char
