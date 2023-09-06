5651d7ea78d7e9f4623fc0495d38b5be9f41e4fb
Elan <elankatheryne@gmail.com>
2023-09-06 04:20:40 +0200

    minor fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,19 +9,19 @@
 * Return: 1 if error else 0
 */
 
-
 int main(int argc, char *argv[])
 {
-if (argc != 3)
+int num1, num2, result;
+if (argc < 3 || argc > 3)
 {
 printf("Error\n");
 return (1);
 }
 
-int num1 = atoi(argv[1]);
-int num2 = atoi(argv[2]);
+num1 = _atoi(argv[1]);
+num2 = _atoi(argv[2]);
 
-int result = num1 * num2;
+result = num1 * num2;
 
 printf("%d\n", result);
 
