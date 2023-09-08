604aee6e4d47cb3573028aaaf8d08c0e6c32662b
Elan <elankatheryne@gmail.com>
2023-09-08 23:53:59 +0200

    minor fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,7 +14,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *result;
-unsigned int len1, len2, i = 0, j = 0;
+unsigned int len1 = 0, len2 = 0, i = 0, j = 0;
 
 while (s1 && s1[len1])
 len1++;
