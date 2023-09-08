3dd0eac22260c8beebf7b02ac34a25501ba32e1d
Elan <elankatheryne@gmail.com>
2023-09-08 23:13:39 +0200

    minor fix realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,25 +13,39 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-if (new_size == 0)
+char *new_ptr;
+char *old_ptr;
+unsigned int i;
+
+if (new_size == old_size)
+return (ptr);
+
+if (new_size == 0 && ptr)
 {
 free(ptr);
 return (NULL);
 }
 
-if (ptr == NULL)
+if (!ptr)
 return (malloc(new_size));
 
-if (new_size == old_size)
-return (ptr);
-
-void *new_ptr = malloc(new_size);
-
-if (new_ptr == NULL)
+new_ptr = malloc(new_size);
+if (!new_ptr)
 return (NULL);
 
-unsigned int min_size = (old_size < new_size) ? old_size : new_size;
-memcpy(new_ptr, ptr, min_size);
+old_ptr = ptr;
+if (new_size < old_size)
+{
+for (i = 0; i < new_size; i++)
+new_ptr[i] = old_ptr[i];
+}
+
+if (new_size > old_size)
+{
+for (i = 0; i < old_size; i++)
+new_ptr[i] = old_ptr[i];
+}
+
 free(ptr);
 
 return (new_ptr);
